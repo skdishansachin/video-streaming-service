1811f2751a85d47aff6ecbe81b26f3759f668230
skdishansachin <skdishansachin@gmail.com>
2024-12-24 17:27:35 +0530

    Add CloudFront distribution and S3 bucket deployment for React web application

--- infra/lib/video-streaming-stack.ts
+++ infra/lib/video-streaming-stack.ts
@@ -1,17 +1,63 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { S3StaticWebsiteOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { Construct } from 'constructs';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
 export class VideoStreamingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const stackName = this.stackName.toLowerCase();
+
     const reactWebApplicationBucket = new s3.Bucket(this, 'ReactWebApplicationBucket', {
-      bucketName: `react-web-application-bucket-${this.stackName}`,
+      bucketName: `react-web-application-bucket-${stackName}`,
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'error.html', // TODO: Create error.html
       autoDeleteObjects: true, // Change on production
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const reactWebApplicationDistribution = new cloudfront.Distribution(this, 'ReactWebApplicationDistribution', {
+      defaultRootObject: 'index.html',
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      defaultBehavior: {
+        origin: new S3StaticWebsiteOrigin(reactWebApplicationBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ]
+    });
+
+    new BucketDeployment(this, 'ReactWebApplicationDeployment', {
+      sources: [Source.asset('../frontend/build')],
+      destinationBucket: reactWebApplicationBucket,
+      distribution: reactWebApplicationDistribution,
+      distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, 'ReactWebApplicationBucketOutputURL', {
+      value: reactWebApplicationDistribution.distributionDomainName,
+      description: 'React Web Application Bucket Output URL',
+      exportName: `${stackName}-ReactWebApplicationBucketOutputURL`,
+    });
+
+    new cdk.CfnOutput(this, 'ReactWebApplicationBucketName', {
+      value: reactWebApplicationBucket.bucketName,
+      description: 'React Web Application Bucket Name',
+      exportName: `${stackName}-ReactWebApplicationBucketName`,
     });
   }
 }
