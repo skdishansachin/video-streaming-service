571412fddf0541bc56193ffccc613117a760220e
skdishansachin <skdishansachin@gmail.com>
2024-12-24 16:58:26 +0530

    Add S3 bucket for React web application in video streaming stack

--- infra/lib/video-streaming-stack.ts
+++ infra/lib/video-streaming-stack.ts
@@ -1,8 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class VideoStreamingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const reactWenApplicationBucket = new s3.Bucket(this, 'ReactWebApplicationBucket', {
+      bucketName: `react-web-application-bucket-${this.stackName}`,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html', // TODO: Create error.html
+      autoDeleteObjects: true, // Change on production
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
