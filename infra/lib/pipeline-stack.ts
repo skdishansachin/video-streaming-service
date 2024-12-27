fd841e669c427efe3526342d4b48b591930fe88e
skdishansachin <skdishansachin@gmail.com>
2024-12-27 18:23:32 +0530

    Update pipeline stack to use ShellStep for synthesis and adjust install commands

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -1,7 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { VideoStreamingStage } from './pipeline-stage';
+import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 
 export class PipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,12 +10,16 @@ export class PipelineStack extends Stack {
 
         const pipeline = new CodePipeline(this, 'ReactAppPipeline', {
             pipelineName: 'ReactAppPipeline',
-            synth: new CodeBuildStep('Synth', {
+            synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('skdishansachin/video-streaming-service', 'main'),
+                installCommands: [
+                    'npm install -g aws-cdk',
+                    'npm install -g pnpm',
+                ],
                 commands: [
                     'cd infra',
-                    'npm install',
-                    'npx cdk synth'
+                    'pnpm install',
+                    'npx cdk deploy',
                 ],
                 primaryOutputDirectory: 'infra/cdk.out',
             })
