9fccc503f6e6d4d4ba681aafcea4cfdd84477095
skdishansachin <skdishansachin@gmail.com>
2024-12-27 19:19:42 +0530

    Update install commands in pipeline stack to use npm ci for consistency

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -2,7 +2,6 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { VideoStreamingStage } from './pipeline-stage';
-import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 
 export class PipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,12 +11,11 @@ export class PipelineStack extends Stack {
             pipelineName: 'ReactAppPipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('skdishansachin/video-streaming-service', 'main'),
-                installCommands: [
-                    'npm install -g aws-cdk',
-                ],
+                installCommands: ['npm install -g aws-cdk'],
                 commands: [
                     'cd infra',
-                    'npm install',
+                    'npm ci',
+                    'npm run build',
                     'npx cdk deploy',
                 ],
                 primaryOutputDirectory: 'infra/cdk.out',
